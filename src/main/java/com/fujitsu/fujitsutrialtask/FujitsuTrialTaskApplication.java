ecb0bf3aa47777beed0764238e50d6c96d919977
mpiirs <mpiirs@ttu.ee>
2024-03-10 16:27:30 +0200

    enabled scheduling for application

--- src/main/java/com/fujitsu/fujitsutrialtask/FujitsuTrialTaskApplication.java
+++ src/main/java/com/fujitsu/fujitsutrialtask/FujitsuTrialTaskApplication.java
@@ -2,8 +2,10 @@ package com.fujitsu.fujitsutrialtask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class FujitsuTrialTaskApplication {
 
     public static void main(String[] args) {
