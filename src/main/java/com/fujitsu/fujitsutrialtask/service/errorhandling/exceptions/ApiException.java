d70579fd956200c4ecddbd24e3a640c335b3d82d
mpiirs <mpiirs@ttu.ee>
2024-03-11 12:49:04 +0200

    replaced previous lombok annotations with data annotation

--- src/main/java/com/fujitsu/fujitsutrialtask/service/errorhandling/exceptions/ApiException.java
+++ src/main/java/com/fujitsu/fujitsutrialtask/service/errorhandling/exceptions/ApiException.java
@@ -1,17 +1,13 @@
 package com.fujitsu.fujitsutrialtask.service.errorhandling.exceptions;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 import org.springframework.http.HttpStatusCode;
 
 
 /*
   Thrown when the application is unable to get new weather data from the API.
  */
-@Getter
-@Setter
-@AllArgsConstructor
+@Data
 public class ApiException extends Exception {
     private final String message;
     private final HttpStatusCode httpStatusCode;
