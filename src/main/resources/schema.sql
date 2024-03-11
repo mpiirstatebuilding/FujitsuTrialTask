a947d4fc945ed0a57602a284304c1591a3490c72
mpiirs <mpiirs@ttu.ee>
2024-03-11 14:33:54 +0200

    changed schema.sql to have WMO_CODE and TIMESTAMP pair as alternate key

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,11 @@
 CREATE TABLE WEATHER_DATA (
     station_name VARCHAR(50) NOT NULL,
-    wmo_code INT,
+    wmo_code INT NOT NULL,
     air_temp DOUBLE,
     wind_speed DOUBLE,
     phenomenon VARCHAR(50),
     timestamp TIMESTAMP NOT NULL,
-    PRIMARY KEY (station_name, timestamp)
+
+    PRIMARY KEY (station_name, timestamp),
+    UNIQUE (wmo_code, timestamp)
 );
