275ad19a5ff1442ec1a16f1f4df4109f9fb69c47
mpiirs <mpiirs@ttu.ee>
2024-03-11 15:04:53 +0200

    @EqualsAndHashCode(callSuper = true) annotation in custom exceptions

--- src/main/java/com/fujitsu/fujitsutrialtask/service/errorhandling/exceptions/ApiException.java
+++ src/main/java/com/fujitsu/fujitsutrialtask/service/errorhandling/exceptions/ApiException.java
@@ -1,12 +1,14 @@
 package com.fujitsu.fujitsutrialtask.service.errorhandling.exceptions;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.springframework.http.HttpStatusCode;
 
 
 /*
   Thrown when the application is unable to get new weather data from the API.
  */
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class ApiException extends Exception {
     private final String message;
--- src/main/java/com/fujitsu/fujitsutrialtask/service/errorhandling/exceptions/ParsingException.java
+++ src/main/java/com/fujitsu/fujitsutrialtask/service/errorhandling/exceptions/ParsingException.java
@@ -1,10 +1,12 @@
 package com.fujitsu.fujitsutrialtask.service.errorhandling.exceptions;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /*
 Thrown when encountering a problem during the parsing of new data.
  */
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class ParsingException extends Exception {
     private final String message;
