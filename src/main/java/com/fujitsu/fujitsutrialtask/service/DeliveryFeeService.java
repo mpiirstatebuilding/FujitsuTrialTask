7be3d83f90afcc02ad080d9c7d4a447820e4d949
mpiirs <mpiirs@ttu.ee>
2024-03-14 17:46:38 +0200

    logic for calculating delivery fee basically in place but unrtested

--- src/main/java/com/fujitsu/fujitsutrialtask/service/DeliveryFeeService.java
+++ src/main/java/com/fujitsu/fujitsutrialtask/service/DeliveryFeeService.java
@@ -1,33 +1,92 @@
 package com.fujitsu.fujitsutrialtask.service;
 
+import com.fujitsu.fujitsutrialtask.repository.WeatherDataEntryRepository;
+import com.fujitsu.fujitsutrialtask.repository.entity.CompositeKey;
+import com.fujitsu.fujitsutrialtask.repository.entity.WeatherDataEntry;
+import com.fujitsu.fujitsutrialtask.service.errorhandling.exceptions.DeliveryFeeException;
+import com.fujitsu.fujitsutrialtask.service.errorhandling.exceptions.WeatherConditionException;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.QueryParameterException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
 @Service
 public class DeliveryFeeService {
+    final WeatherDataEntryRepository repository;
+    @Value("#{${delivery-fee.query.city-to-station}}")
+    HashMap<String, String> stationDictionary;
+    @Value("#{${delivery-fee.rbf.city}}")
+    HashMap<String, Float> cityRbf;
+    @Value("#{${delivery-fee.rbf.vehicle}}")
+    HashMap<String, Float> vehicleRbf;
+    @Value("#{'${delivery-fee.atef}'.split(',')}")
+    List<Float> atef;
+    @Value("#{'${delivery-fee.wsef}'.split(',')}")
+    List<Float> wsef;
+    @Value("#{${delivery-fee.wpef}}")
+    HashMap<String, Float> wpef;
+    public Float getDeliveryFee(String city, String vehicle, String timestampString) throws DeliveryFeeException, WeatherConditionException {
+        Float deliveryFee = 0.0f;
 
-    public enum City {
-        Tallinn("Tallinn-Harku", 4.0),
-        Tartu("Tartu-Tõravere", 3.5),
-        Parnu("Pärnu", 2.5);
+        // get city fee
+        if (!cityRbf.containsKey(city)) throw new QueryParameterException("Unknown city!");
+        deliveryFee += cityRbf.get(city);
 
-        final String station;
-        final Double RBF;
+        // subtract by vehicle
+        if (!vehicleRbf.containsKey(vehicle)) throw new QueryParameterException("Unknown vehicle!");
+        deliveryFee -= vehicleRbf.get(vehicle);
 
-        City(String station, Double RBF) {
-            this.station = station;
-            this.RBF = RBF;
+        // extra fee rules are only outlined for scooters and bikes, so if the vehicle type is car there's no need to do anymore
+        if (!vehicle.equals("car")) {
+            Optional<WeatherDataEntry> entryOptional = null;
+            String station = stationDictionary.get(city);
+            if (timestampString != null) {
+                long timestampLong = Long.parseLong(timestampString) * 1000;
+                Timestamp timestamp = new Timestamp(timestampLong);
+                entryOptional = repository.findById(new CompositeKey(station, timestamp));
+            }
+
+            if (entryOptional.isEmpty()) { // it would be empty if either the timestamp was not provided or the last clause was unable to find a suitable entry
+                entryOptional = repository.findTopByCompositeKeyStationNameOrderByCompositeKeyTimestampDesc(station);
+                if (entryOptional.isEmpty()) throw new DeliveryFeeException("Could not find relevant weather data!");
+            }
+
+            WeatherDataEntry entry = entryOptional.get();
+
+            // extra fees based on air temperature
+            Double airTemperature = entry.getAirTemperature();
+            if (airTemperature < -10.0) {
+                deliveryFee += atef.get(0);
+            } else if (airTemperature <= 0.0) {
+                deliveryFee += atef.get(1);
+            }
+
+            String phenomenon = entry.getPhenomenon().toLowerCase();
+            if (phenomenon.contains("snow") || phenomenon.contains("sleet")) {
+                deliveryFee += wpef.get("snow");
+            } else if (phenomenon.contains("rain") || phenomenon.contains("shower")) { // if it was a snow shower, it would have been dealt with in the last clause
+                deliveryFee += wpef.get("rain");
+            } else if (phenomenon.contains("glaze") || phenomenon.contains("hail") || phenomenon.contains("thunder")) {
+                throw new WeatherConditionException("Usage of selected vehicle type is forbidden!");
+            }
+
+            // extra fees based on wind speed
+            if (vehicle.equals("bike")) {
+                Double windSpeed = entry.getWindSpeed();
+                if (windSpeed > 20.0) {
+                    throw new WeatherConditionException("Usage of selected vehicle type is forbidden!");
+                } else if (windSpeed >= 10) {
+                    deliveryFee += wsef.get(0);
+                }
+            }
         }
+
+        return deliveryFee;
     }
-
-    public enum Vehicle {
-        Car(0.0), Scooter(0.5), Bike(1.0);
-        final Double RBFSubtract; // this will be subtracted from the city's RBF
-
-        Vehicle(Double RBFSubtract) {
-            this.RBFSubtract = RBFSubtract;
-        }
-
-        // TODO: make submethods for specified vehicle types as outlined in extra fee business rules
-    }
-
 }
