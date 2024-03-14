d209ce195f4d7ab86b1dd1cb774db00c816be5ce
mpiirs <mpiirs@ttu.ee>
2024-03-14 16:18:59 +0200

    added method for getting delivery fee based on request parameters

--- src/main/java/com/fujitsu/fujitsutrialtask/controller/DeliveryFeeController.java
+++ src/main/java/com/fujitsu/fujitsutrialtask/controller/DeliveryFeeController.java
@@ -2,10 +2,18 @@ package com.fujitsu.fujitsutrialtask.controller;
 
 import com.fujitsu.fujitsutrialtask.service.DeliveryFeeService;
 import lombok.Data;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Data
 @RestController
 public class DeliveryFeeController {
     final DeliveryFeeService deliveryFeeService;
+
+    @GetMapping("/api/deliveryfee")
+    public String getDeliveryFee(@RequestParam(name = "city") String city, @RequestParam(name = "vehicle") String vehicle,
+                                 @RequestParam(required = false, name = "timestamp") String timestamp) {
+        return deliveryFeeService.getDeliveryFee(city, vehicle, timestamp);
+    }
 }
