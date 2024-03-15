07775b1a81cb9fdc3ac43e1fa7cb2d3a8a2e578c
mpiirs <mpiirs@taltech.ee>
2024-03-15 19:42:02 +0200

    finished exception handler

--- src/main/java/com/fujitsu/fujitsutrialtask/service/errorhandling/ErrorHandler.java
+++ src/main/java/com/fujitsu/fujitsutrialtask/service/errorhandling/ErrorHandler.java
@@ -1,20 +1,31 @@
 package com.fujitsu.fujitsutrialtask.service.errorhandling;
 
 
-import com.fujitsu.fujitsutrialtask.service.errorhandling.exceptions.ApiException;
-import com.fujitsu.fujitsutrialtask.service.errorhandling.exceptions.ParsingException;
+import com.fujitsu.fujitsutrialtask.service.errorhandling.exceptions.*;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.QueryParameterException;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.time.ZonedDateTime;
+
 @ControllerAdvice
 @Slf4j
 public class ErrorHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler({ApiException.class, ParsingException.class})
-    public ResponseEntity<Object> handleWeatherDataServiceExceptions(Exception e) {
-        return new ResponseEntity<>(, new HttpHeaders())
+    public void handleWeatherDataServiceExceptions(Exception e) {
+        log.error(e.getMessage(), e);
+    }
+
+    @ExceptionHandler({DeliveryFeeException.class, WeatherConditionException.class, QueryParameterException.class})
+    public ResponseEntity<Object> handleDeliveryFeeServiceExceptions(Exception e) {
+        HttpStatus badRequest = HttpStatus.BAD_REQUEST;
+        log.error("Failed to provide delivery fee. Cause: " + e.getMessage());
+        BadRequestException badRequestException = new BadRequestException(e.getMessage(), badRequest, ZonedDateTime.now());
+        return new ResponseEntity<>(badRequestException, new HttpHeaders(), badRequest);
     }
 }
