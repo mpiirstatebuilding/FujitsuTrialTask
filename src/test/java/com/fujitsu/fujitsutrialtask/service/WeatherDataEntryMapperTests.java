83d102227c19f4172f616be7158365e30df2307c
mpiirs <mpiirs@taltech.ee>
2024-03-21 14:42:21 +0200

    mocks initialized

---
+++ src/test/java/com/fujitsu/fujitsutrialtask/service/WeatherDataEntryMapperTests.java
@@ -0,0 +1,62 @@
+package com.fujitsu.fujitsutrialtask.service;
+
+import com.fujitsu.fujitsutrialtask.service.errorhandling.exceptions.ParsingException;
+import com.fujitsu.fujitsutrialtask.service.mapper.WeatherDataEntryMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.w3c.dom.Element;
+
+import java.sql.Timestamp;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class WeatherDataEntryMapperTests {
+    @Autowired
+    private WeatherDataEntryMapper mapper;
+    private static String stationName = "Pakri";
+    private static String wmoCode = "26029";
+    private static String airTemp = "2.9";
+    private static String windSpeed = "2.5";
+    private static String phenomenon = "Mist";
+    private static Timestamp timestamp = new Timestamp(Long.parseLong("1711020886000"));
+    private static Element stationElement = mock(Element.class);
+    private static Element stationElementNoWmoCode = mock(Element.class);
+
+    @BeforeAll
+    static void initializeStationElement() {
+        when(stationElement.getElementsByTagName("wmocode").item(0).getTextContent()).thenReturn(wmoCode);
+        when(stationElement.getElementsByTagName("airtemperature").item(0).getTextContent()).thenReturn(airTemp);
+        when(stationElement.getElementsByTagName("windspeed").item(0).getTextContent()).thenReturn(windSpeed);
+        when(stationElement.getElementsByTagName("phenomenon").item(0).getTextContent()).thenReturn(phenomenon);
+
+        when(stationElementNoWmoCode.getElementsByTagName("wmocode").item(0).getTextContent()).thenReturn("");
+        when(stationElementNoWmoCode.getElementsByTagName("airtemperature").item(0).getTextContent()).thenReturn(airTemp);
+        when(stationElementNoWmoCode.getElementsByTagName("windspeed").item(0).getTextContent()).thenReturn(windSpeed);
+        when(stationElementNoWmoCode.getElementsByTagName("phenomenon").item(0).getTextContent()).thenReturn(phenomenon);
+    }
+
+    @Test
+    void testThrowsExceptionIfNameAbsent() {
+        Assertions.assertThrows(ParsingException.class, () -> mapper.elementToWeatherDataEntry(stationElement, "", timestamp));
+        Assertions.assertThrows(ParsingException.class, () -> mapper.elementToWeatherDataEntry(stationElement, null, timestamp));
+    }
+
+    @Test
+    void testThrowsExceptionIfTimestampAbsent() {
+        Assertions.assertThrows(ParsingException.class, () -> mapper.elementToWeatherDataEntry(stationElement, stationName, null));
+    }
+
+    @Test
+    void testThrowsExceptionIfWmoCodeAbsent() {
+        Assertions.assertThrows(ParsingException.class, () -> mapper.elementToWeatherDataEntry(stationElementNoWmoCode, stationName, timestamp));
+    }
+
+}
