f20cf2f58aa5663da1279c6eeeee3c7731fbe77e
mpiirs <mpiirs@taltech.ee>
2024-03-22 21:00:32 +0200

    reformatting

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,11 @@
-CREATE TABLE WEATHER_DATA (
+CREATE TABLE WEATHER_DATA
+(
     station_name VARCHAR(50) NOT NULL,
-    wmo_code VARCHAR(50) NOT NULL,
-    air_temp DOUBLE,
-    wind_speed DOUBLE,
-    phenomenon VARCHAR(50),
-    timestamp TIMESTAMP NOT NULL,
+    wmo_code     VARCHAR(50) NOT NULL,
+    air_temp     DOUBLE,
+    wind_speed   DOUBLE,
+    phenomenon   VARCHAR(50),
+    timestamp    TIMESTAMP   NOT NULL,
 
     PRIMARY KEY (station_name, timestamp),
     UNIQUE (wmo_code, timestamp)
--- src/test/java/com/fujitsu/fujitsutrialtask/controller/RestControllerTest.java
+++ src/test/java/com/fujitsu/fujitsutrialtask/controller/RestControllerTest.java
@@ -1,6 +1,9 @@
 package com.fujitsu.fujitsutrialtask.controller;
 
 
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
 import com.fujitsu.fujitsutrialtask.service.DeliveryFeeService;
 import com.fujitsu.fujitsutrialtask.service.errorhandling.exceptions.DeliveryFeeException;
 import com.fujitsu.fujitsutrialtask.service.errorhandling.exceptions.WeatherConditionException;
@@ -15,81 +18,80 @@ import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
 @WebMvcTest(DeliveryFeeController.class)
 class RestControllerTest {
-    @Autowired
-    private MockMvc mvc;
-    @MockBean
-    DeliveryFeeService deliveryFeeService;
-    private final int badRequest = HttpStatus.BAD_REQUEST.value();
-    private final int ok = HttpStatus.OK.value();
-    @Test
-    void testGetDeliveryFeeNoTimestamp() throws Exception {
-        given(deliveryFeeService.getDeliveryFee("tallinn", "scooter", null))
-                .willReturn(3.5f);
 
-        MockHttpServletResponse response = mvc.perform(
-                        get("/api/deliveryfee?city=tallinn&vehicle=scooter")
-                                .accept(MediaType.APPLICATION_JSON))
-                .andReturn().getResponse();
+  @Autowired
+  private MockMvc mvc;
+  @MockBean
+  DeliveryFeeService deliveryFeeService;
+  private final int badRequest = HttpStatus.BAD_REQUEST.value();
+  private final int ok = HttpStatus.OK.value();
 
-        Assertions.assertEquals(ok, response.getStatus());
-        Assertions.assertEquals("3.5", response.getContentAsString());
-    }
+  @Test
+  void testGetDeliveryFeeNoTimestamp() throws Exception {
+    given(deliveryFeeService.getDeliveryFee("tallinn", "scooter", null))
+        .willReturn(3.5f);
 
-    @Test
-    void testGetDeliveryFeeWithTimestamp() throws Exception {
-        given(deliveryFeeService.getDeliveryFee("tallinn", "scooter", "1711124333"))
-                .willReturn(4.5f);
+    MockHttpServletResponse response = mvc.perform(
+            get("/api/deliveryfee?city=tallinn&vehicle=scooter")
+                .accept(MediaType.APPLICATION_JSON))
+        .andReturn().getResponse();
 
-        MockHttpServletResponse response = mvc.perform(
-                        get("/api/deliveryfee?city=tallinn&vehicle=scooter&timestamp=1711124333")
-                                .accept(MediaType.APPLICATION_JSON))
-                .andReturn().getResponse();
+    Assertions.assertEquals(ok, response.getStatus());
+    Assertions.assertEquals("3.5", response.getContentAsString());
+  }
 
-        Assertions.assertEquals(ok, response.getStatus());
-        Assertions.assertEquals("4.5", response.getContentAsString());
-    }
+  @Test
+  void testGetDeliveryFeeWithTimestamp() throws Exception {
+    given(deliveryFeeService.getDeliveryFee("tallinn", "scooter", "1711124333"))
+        .willReturn(4.5f);
 
-    @Test
-    void testDeliveryFeeException() throws Exception {
-        given(deliveryFeeService.getDeliveryFee("tallinn", "scooter", "1"))
-                .willThrow(DeliveryFeeException.class);
+    MockHttpServletResponse response = mvc.perform(
+            get("/api/deliveryfee?city=tallinn&vehicle=scooter&timestamp=1711124333")
+                .accept(MediaType.APPLICATION_JSON))
+        .andReturn().getResponse();
 
-        MockHttpServletResponse response = mvc.perform(
-                        get("/api/deliveryfee?city=tallinn&vehicle=scooter&timestamp=1")
-                                .accept(MediaType.APPLICATION_JSON))
-                .andReturn().getResponse();
+    Assertions.assertEquals(ok, response.getStatus());
+    Assertions.assertEquals("4.5", response.getContentAsString());
+  }
 
-        Assertions.assertEquals(badRequest, response.getStatus());
-    }
+  @Test
+  void testDeliveryFeeException() throws Exception {
+    given(deliveryFeeService.getDeliveryFee("tallinn", "scooter", "1"))
+        .willThrow(DeliveryFeeException.class);
 
-    @Test
-    void testWeatherConditionException() throws Exception {
-        given(deliveryFeeService.getDeliveryFee("tallinn", "scooter", "2"))
-                .willThrow(WeatherConditionException.class);
+    MockHttpServletResponse response = mvc.perform(
+            get("/api/deliveryfee?city=tallinn&vehicle=scooter&timestamp=1")
+                .accept(MediaType.APPLICATION_JSON))
+        .andReturn().getResponse();
 
-        MockHttpServletResponse response = mvc.perform(
-                        get("/api/deliveryfee?city=tallinn&vehicle=scooter&timestamp=2")
-                                .accept(MediaType.APPLICATION_JSON))
-                .andReturn().getResponse();
+    Assertions.assertEquals(badRequest, response.getStatus());
+  }
 
-        Assertions.assertEquals(badRequest, response.getStatus());
-    }
+  @Test
+  void testWeatherConditionException() throws Exception {
+    given(deliveryFeeService.getDeliveryFee("tallinn", "scooter", "2"))
+        .willThrow(WeatherConditionException.class);
 
-    @Test
-    void testQueryParameterException() throws Exception {
-        given(deliveryFeeService.getDeliveryFee("paris", "tractor", null))
-                .willThrow(QueryParameterException.class);
+    MockHttpServletResponse response = mvc.perform(
+            get("/api/deliveryfee?city=tallinn&vehicle=scooter&timestamp=2")
+                .accept(MediaType.APPLICATION_JSON))
+        .andReturn().getResponse();
 
-        MockHttpServletResponse response = mvc.perform(
-                        get("/api/deliveryfee?city=paris&vehicle=tractor")
-                                .accept(MediaType.APPLICATION_JSON))
-                .andReturn().getResponse();
+    Assertions.assertEquals(badRequest, response.getStatus());
+  }
 
-        Assertions.assertEquals(badRequest, response.getStatus());
-    }
+  @Test
+  void testQueryParameterException() throws Exception {
+    given(deliveryFeeService.getDeliveryFee("paris", "tractor", null))
+        .willThrow(QueryParameterException.class);
+
+    MockHttpServletResponse response = mvc.perform(
+            get("/api/deliveryfee?city=paris&vehicle=tractor")
+                .accept(MediaType.APPLICATION_JSON))
+        .andReturn().getResponse();
+
+    Assertions.assertEquals(badRequest, response.getStatus());
+  }
 }
